fd7b31fcfe8b71930b4807c4dc619d99a2e2b31a
Anthony Fecondo <afecondo@udel.edu>
2017-11-02 13:19:00 -0400

    bs added

---
+++ 11-2-2017/bs-basics.js
@@ -0,0 +1,8 @@
+function main () {                    //will need this code to start
+  $('#success-button').click(function () {
+    $('#exampleModal').modal('show');
+  });
+}
+
+
+$(document).ready(main);             //will need this code to start
